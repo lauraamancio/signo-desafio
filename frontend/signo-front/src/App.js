a4b206819419390519b1452e6c1ff5874c56ba96
Laura Amancio <lauran.amancio@yahoo.com.br>
2022-08-08 14:40:39 -0300

    ajustes de tema

--- frontend/signo-front/src/App.js
+++ frontend/signo-front/src/App.js
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "@material-ui/core";
 import React from "react"
 import Router from "./routes/Router";
+import theme from "./constants/theme"
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Router/>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- frontend/signo-front/src/pages/LoginPage/LoginPage.js
+++ frontend/signo-front/src/pages/LoginPage/LoginPage.js
@@ -1,14 +1,15 @@
 import React from "react"
 import {goToFeedPage, goToSignUpPage} from "../../routes/coordinator"
 import { useNavigate } from "react-router-dom"
+import { Button } from "@material-ui/core"
 
 const LoginPage = () => {
     const navigate = useNavigate()
     return(
         <div>
             <h1>Login Page</h1>
-            <button onClick={() => goToFeedPage(navigate)} > Entrar </button>
-            <button onClick={() => goToSignUpPage(navigate)}> Cadastrar </button>
+            <Button color = "primary" variant="contained" onClick={() => goToFeedPage(navigate)} > Entrar </Button>
+            <Button color = "primary" variant="contained" onClick={() => goToSignUpPage(navigate)}> Cadastrar </Button>
         </div>
     )
 }
